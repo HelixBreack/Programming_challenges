64f0dc47bb8571727d8e18a37dfb0d852c1676b9
Ivan Mostovich <106533891+HelixBreack@users.noreply.github.com>
2022-05-30 21:54:20 +0300

    Create 5_Comparing_Numbers.sh

---
+++ Bash/Easy/5_Comparing_Numbers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Given two integers, X and Y 
+# X is less than Y
+# X is greater than Y
+# X is equal to Y
+
+read X
+read Y
+if [ "$Y" -gt "$X" ]
+then
+  echo "X is less than Y"
+elif [ "$X" -gt "$Y" ]
+then
+  echo "X is greater than Y"
+else
+  echo "X is equal to Y"
+fi
