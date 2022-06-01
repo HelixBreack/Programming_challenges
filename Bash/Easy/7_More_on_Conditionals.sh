51e0e3bcefee41364e04c1eeb9838a0966cf0a2c
Ivan Mostovich <106533891+HelixBreack@users.noreply.github.com>
2022-06-01 17:52:20 +0300

    Create 7_More_on_Conditionals.sh

---
+++ Bash/Easy/7_More_on_Conditionals.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Given three integers representing the three sides of a triangle, identify whether the triangle is scalene, isosceles, or equilateral.
+# If all three sides are equal, output EQUILATERAL.
+# Otherwise, if any two sides are equal, output ISOSCELES.
+# Otherwise, output SCALENE.
+
+read X
+read Y
+read Z
+if [ $X == $Y ] && [ $X == $Z ]
+then echo "EQUILATERAL"
+elif [ $X == $Y ] || [ $X == $Z ] || [ $Y == $Z ]
+then echo "ISOSCELES"
+else echo "SCALENE"
+fi
