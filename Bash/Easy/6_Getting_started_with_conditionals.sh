a2c18598a073d873ec0f9a388673d8ddf0cd6004
Ivan Mostovich <106533891+HelixBreack@users.noreply.github.com>
2022-06-01 14:39:47 +0300

    Update 6_Getting_started_with_conditionals.sh

--- Bash/Easy/6_Getting_started_with_conditionals.sh
+++ Bash/Easy/6_Getting_started_with_conditionals.sh
@@ -5,11 +5,22 @@
 # If the character is 'N' or 'n' display "NO".
 # No other character will be provided as input.
 
+# Solution 1
 read letter
 if [ $letter == "Y" ] || [ $letter == "y" ]
 then
   echo "YES"
 elif [ $letter == "N" ] || [ $letter == "n" ]
 then
+  echo "NO"
+fi
+
+
+# Solution 2
+read letter
+if [ $letter == "Y" ] || [ $letter == "y" ]
+then
+  echo "YES"
+else
   echo "NO"
 fi
